efc8c836256bb58e43d7d624f95fa0887acf46a2
Wen Kai Ng <wn2195@columbia.edu>
2022-11-02 01:54:08 -0400

    Test added.

--- tests/test_cipher_wn2195.py
+++ tests/test_cipher_wn2195.py
@@ -1,1 +1,10 @@
 from cipher_wn2195 import cipher_wn2195
+
+import pytest
+
+def cipher_test():
+	test = "Barcelona"
+	shift = 1
+	expected = "Cvsdfmpob"
+	actual = cipher_wn2195.cipher(test, shift, encrypt=True)
+	assert actual == expected, "Should be Cvsdfmpob"
